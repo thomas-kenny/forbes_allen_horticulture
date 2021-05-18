92c72990a92666cdf747a00678b4e62a40687e45
Thomas Kenny <thomasarthurkenny@gmail.com>
2021-05-18 13:01:09 +0100

    Bigger cards used for index pages, link to show pages and a footer with social medias added.

--- app/controllers/design_projects_controller.rb
+++ app/controllers/design_projects_controller.rb
@@ -2,4 +2,8 @@ class DesignProjectsController < ApplicationController
   def index
     @design_projects = DesignProject.all
   end
+
+  def show
+    @design_project = DesignProject.find(params[:id])
+  end
 end
--- app/controllers/maintenance_projects_controller.rb
+++ app/controllers/maintenance_projects_controller.rb
@@ -2,4 +2,8 @@ class MaintenanceProjectsController < ApplicationController
   def index
     @maintenance_projects = MaintenanceProject.all
   end
+
+  def show
+    @maintenance_project = MaintenanceProject.find(params[:id])
+  end
 end
