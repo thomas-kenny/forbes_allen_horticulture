2bd073d54e5d85b17c768273180f3a214949c8b3
Thomas Kenny <thomasarthurkenny@gmail.com>
2021-09-01 09:18:57 +0100

    active class JS now working on show pages

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,10 +1,12 @@
 const initUpdateActiveClassOnClick = () => {
   const currentLocation = location.href
+  // console.log(currentLocation)
   const navbar = document.querySelector('.navbar-nav')
   const header = navbar.querySelectorAll(".nav-link");
   const menuLength = header.length;
   for (let i = 0; i < menuLength; i++) {
-    if(header[i].href === currentLocation) {
+      console.log(header[i].href)
+    if(currentLocation.includes(header[i].href)) {
       header[i].classList.add("active")
     }
   }
